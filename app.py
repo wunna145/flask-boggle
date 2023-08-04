b2a9431d25909c7812803d73f4b6adf47554939d
Zaw Wunna <zawwunna@Zaws-Air.lan>
2023-08-04 00:17:21 -0700

    adding files

---
+++ app.py
@@ -0,0 +1,35 @@
+from boggle import Boggle
+from flask import Flask, request, render_template, jsonify, session
+
+boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "thedoorisbanana"
+
+@app.route("/")
+def home():
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template("index.html", board = board, highscore = highscore, nplays = nplays)
+
+@app.route("/check-word")
+def check_word():
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result' : response})
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays+1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord = score > highscore)
